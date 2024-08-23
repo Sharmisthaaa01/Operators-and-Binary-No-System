914d2179beed180fd1d761a66f983c11ec3b7506
Sharmistha Sarkar <sharmisthasarkar719@gmail.com>
2024-08-23 14:16:04 +0530

    Initial commit

---
+++ Operators_1.java
@@ -0,0 +1,11 @@
+public class Operators_1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
